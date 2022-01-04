19d0e0ca4180de2907bae5a29eaa00f4cd7f4f75
João Paulo <45145741+joao5142@users.noreply.github.com>
2022-01-04 18:57:26 -0300

    Delete main.js

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-
-       const iconsHeartSvg = document.querySelectorAll(".svg");
-
-       iconsHeartSvg.forEach(icon => {
-           icon.addEventListener("click", () => {
-               icon.classList.toggle("active");
-              
-           });
-       });
